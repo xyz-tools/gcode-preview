caa65f6709c5500b25845e52f406e983857156d4
Remco Veldkamp <remcoder@gmail.com>
2025-07-21 17:04:21 +0200

    Use concise format

--- src/layer.ts
+++ src/layer.ts
@@ -6,29 +6,18 @@ import { Path } from './path';
  * Contains information about the layer number, paths, height, and Z position
  */
 export class Layer {
-  /** Layer number (0-based index) */
-  public layer: number;
-  /** Array of paths in this layer */
-  public paths: Path[];
-  /** Line number in the G-code file where this layer starts */
-  public lineNumber: number;
-  /** Height of this layer */
-  public height: number = 0;
-  /** Z position of this layer */
-  public z: number = 0;
-
   /**
    * Creates a new Layer instance
-   * @param layer - Layer number
    * @param paths - Array of paths in this layer
    * @param lineNumber - Line number in G-code file
    * @param height - Layer height (default: 0)
    * @param z - Z position (default: 0)
    */
-  constructor(paths: Path[], lineNumber: number, height: number = 0, z: number = 0) {
-    this.paths = paths;
-    this.lineNumber = lineNumber;
-    this.height = height;
-    this.z = z;
-  }
+  constructor(
+    public paths: Path[],
+    public lineNumber: number,
+    public height: number = 0,
+    public z: number = 0,
+    public layer: number = 0
+  ) {}
 }
